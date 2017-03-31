42e4a5ed6f0471a7761a043df086f7e9c14a10fe
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-03-31 11:50:39 +0800

    set up CSS minification and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 const concat = require('gulp-concat');
+
+const filter = require('gulp-filter');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync');
 const story = require('./story_transformer');
 
 const outFolder = 'docs';
@@ -17,8 +21,12 @@ gulp.task('template', () => {
 
 gulp.task('scss', () => {
   return gulp.src(`${srcFolder}/scss/app.scss`)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(outFolder));
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(outFolder))
+    .pipe(filter('**/*.css'))
+    .pipe(browserSync.stream())
 });
 
 gulp.task('js', () => {
@@ -28,3 +36,17 @@ gulp.task('js', () => {
 });
 
 gulp.task('build', ['template', 'scss', 'js']);
+
+// Build is a pre-req so that we start serving the latest version of files
+gulp.task('serve', ['build'], () => {
+  // Start the browser sync server
+  browserSync({
+    server: {
+      baseDir: 'docs',
+    },
+  });
+  
+  // Watch for changes to SCSS files and trigger the build process when 
+  // they are updated
+  return gulp.watch(`${srcFolder}/scss/**/*.scss`, ['scss']);
+});
